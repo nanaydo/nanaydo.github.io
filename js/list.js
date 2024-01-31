3a2874b9ca486a695f78c7dfdd478e6df5ccbcaf
nanaydo <156270818+nanaydo@users.noreply.github.com>
2024-01-31 14:46:13 -0600

    Update list.js

--- js/list.js
+++ js/list.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", () => {
         clone.querySelector("[data-id='title']").textContent = "NAME: Product";
         clone.querySelector("[data-id='status']").textContent = "STATUS: Available";
         clone.querySelector("[data-id='species']").textContent = "SPECIES: Plant";
-        clone.querySelector("[data-id='image']").src = "https://rickandmortyapi.com/api/location/1"; 
+        clone.querySelector("[data-id='image']").src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fmag.elcomercio.pe%2Ffama%2Frick-and-morty-explicacion-del-verdadero-origen-de-rick-sanchez-serie-de-hbo-max-nnda-nnlt-noticia%2F&psig=AOvVaw1T1TuiU__eByZejXuWWse5&ust=1706820284538000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLDn3pi_iIQDFQAAAAAdAAAAABAF"; 
         itemList.appendChild(clone);
     });
 
