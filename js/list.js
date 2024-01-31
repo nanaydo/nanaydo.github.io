6b0d24c74520376d3234cc9646d9d23c7f6753d1
nanaydo <156270818+nanaydo@users.noreply.github.com>
2024-01-31 15:00:49 -0600

    Update list.js

--- js/list.js
+++ js/list.js
@@ -5,10 +5,10 @@ document.addEventListener("DOMContentLoaded", () => {
         var total = itemList.childElementCount + 1;
         var clone = template.content.cloneNode(true);
         clone.querySelector("[data-id='number']").textContent = `${total}`;
-        clone.querySelector("[data-id='title']").textContent = "NAME: Product";
-        clone.querySelector("[data-id='status']").textContent = "STATUS: Available";
-        clone.querySelector("[data-id='species']").textContent = "SPECIES: Plant";
-        clone.querySelector("[data-id='image']").src = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fmag.elcomercio.pe%2Ffama%2Frick-and-morty-explicacion-del-verdadero-origen-de-rick-sanchez-serie-de-hbo-max-nnda-nnlt-noticia%2F&psig=AOvVaw1T1TuiU__eByZejXuWWse5&ust=1706820284538000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCLDn3pi_iIQDFQAAAAAdAAAAABAF"; 
+        clone.querySelector("[data-id='title']").textContent = "NAME: Rick Sanchez";
+        clone.querySelector("[data-id='status']").textContent = "STATUS: Vivo";
+        clone.querySelector("[data-id='species']").textContent = "SPECIES: Humano";
+        clone.querySelector("[data-id='image']").src = "https://elcomercio.pe/resizer/4WQWP7E1GEiSxQgSDeKSaLYa0eo=/580x330/smart/filters:format(jpeg):quality(90)/cloudfront-us-east-1.images.arcpublishing.com/elcomercio/KRKOINCP7JGR7KWT4HNBDSJPDA.jpg"; 
         itemList.appendChild(clone);
     });
 
