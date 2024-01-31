dab3a8f3dd2e8c57172db98deae78c1df2932f6b
nanaydo <156270818+nanaydo@users.noreply.github.com>
2024-01-31 14:38:10 -0600

    Update list.js

--- js/list.js
+++ js/list.js
@@ -5,12 +5,16 @@ document.addEventListener("DOMContentLoaded", () => {
         var total = itemList.childElementCount + 1;
         var clone = template.content.cloneNode(true);
         clone.querySelector("[data-id='number']").textContent = `${total}`;
-        clone.querySelector("[data-id='title']").textContent = "Product";
-        clone.querySelector("[data-id='content']").textContent = "Hola";
+        clone.querySelector("[data-id='title']").textContent = "NAME: Product";
+        clone.querySelector("[data-id='status']").textContent = "STATUS: Available";
+        clone.querySelector("[data-id='species']").textContent = "SPECIES: Plant";
+        clone.querySelector("[data-id='image']").src = "https://rickandmortyapi.com/api/location/1"; 
         itemList.appendChild(clone);
     });
+
     document.querySelector(".btn.btn-light").addEventListener("click", event => {
         var itemList = document.getElementById("my-list");
         itemList.replaceChildren();
     });
 });
+
