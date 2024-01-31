bd2c89254e4f2ca58e67971b813a16f4c3ccfcdf
nanaydo <156270818+nanaydo@users.noreply.github.com>
2024-01-31 13:01:32 -0600

    Update list.js

--- js/list.js
+++ js/list.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
         var clone = template.content.cloneNode(true);
         clone.querySelector("[data-id='number']").textContent = `${total}`;
         clone.querySelector("[data-id='title']").textContent = "Product";
-        clone.querySelector("[data-id='content']").textContent = "It's a new item"";
+        clone.querySelector("[data-id='content']").textContent = "It's a new item";
         itemList.appendChild(clone);
     });
     document.querySelector(".btn.btn-light").addEventListener("click", event => {
