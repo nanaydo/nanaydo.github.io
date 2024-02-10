0515ac461e7ff00007922be287da16ed7af6f18c
nanaydo <156270818+nanaydo@users.noreply.github.com>
2024-02-10 02:24:57 -0600

    Update list.js

--- js/list.js
+++ js/list.js
@@ -1,21 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
     document.querySelector(".btn.btn-primary").addEventListener("click", event => {
-        fetch('https://rickandmortyapi.com/api/character').then((response) =>{
-            if(response.ok){
-                
-
-        
-        var itemList = document.getElementById("my-list");
-        var template = document.getElementById("list-template");
-        var total = itemList.childElementCount + 1;
-        var clone = template.content.cloneNode(true);
-        clone.querySelector("[data-id='number']").textContent = `${total}`;
-        clone.querySelector("[data-id='Id']").textContent = "Id: ";
-        clone.querySelector("[data-id='name']").textContent = "Name: Rick Sanchez";
-        clone.querySelector("[data-id='status']").textContent = "status: Vivo";
-        clone.querySelector("[data-id='species']").textContent = "species: Humano";
-        clone.querySelector("[data-id='image']").src = "https://hips.hearstapps.com/hmg-prod/images/rick-and-morty-image-1662104014.jpg?crop=0.315xw:0.560xh;0.351xw,0.168xh&resize=980:*"; 
-        itemList.appendChild(clone);
+        fetch('https://rickandmortyapi.com/api/character')
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error('HTTP error! status: ${response.status}');
+                }
+                return response.json();
+            })
+            .then((response) => {
+                var itemList = document.getElementById("my-list");
+                var template = document.getElementById("list-template");
+                response.results.forEach(element => {
+                    var clone = template.content.cloneNode(true);
+                    clone.querySelector(".col-xs-12").setAttribute("data-detail", element.id);
+                    clone.querySelector("[data-id='name']").textContent = element.name;
+                    clone.querySelector("[data-id='status']").textContent = element.status;
+                    clone.querySelector("[data-id='species']").textContent = element.species;
+                    clone.querySelector("[data-id='image']").src = element.image;
+                    itemList.appendChild(clone);
+                });
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
     });
 
     document.querySelector(".btn.btn-light").addEventListener("click", event => {
@@ -23,4 +30,5 @@ document.addEventListener("DOMContentLoaded", () => {
         itemList.replaceChildren();
     });
 });
+
 
