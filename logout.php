eeaa8027187c88c08ef34eab10df13991fac091b
nanaydo <156270818+nanaydo@users.noreply.github.com>
2024-04-08 20:41:23 -0600

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,6 +1,9 @@
 <?php
+// Iniciar sesión si no está iniciada
+session_start();
 
 // Destruir todas las variables de sesión
+session_unset();
 session_destroy();
 
 // Redirigir al usuario a la página de inicio de sesión
